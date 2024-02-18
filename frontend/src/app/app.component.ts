07524dc886ec0874ae0c9b25c9b09215bc517ccd
esmiz-dream <esmi1920@gmail.com>
2024-02-18 21:50:31 +0300

    search bar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, isDevMode } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {MaterialModule} from './shared/Material.Module';
 import { TransactionTableComponent } from './components/transaction-table/transaction-table.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import { TransactionEffects } from './store/effects/transaction.Effects';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +17,10 @@ import { SearchBarComponent } from './components/search-bar/search-bar.component
     RouterOutlet,
     MaterialModule,
     TransactionTableComponent,
-    SearchBarComponent
+    SearchBarComponent,
+    // StoreModule.forRoot(AppState),
+    // StoreDevtoolsModule.instrument({maxAge:false, logOnly: !isDevMode}),
+    // EffectsModule.forRoot([TransactionEffects])
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
